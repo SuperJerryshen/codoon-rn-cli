384087bb0799cabb5f475e2ae7ca679195e37344
沈经纬 <codoon@C02TD9VHFVH3.local>
2018-09-30 16:50:05 +0800

    feat(lib/select): 新增选择页面命令，使用后修改pageEntry

--- bin/codoon-rn.js
+++ bin/codoon-rn.js
@@ -9,8 +9,15 @@ const log = console.log;
 program.version('0.0.1');
 
 // 开始运行项目
-program.command('start').action(() => {
-  require('../lib/start')();
-});
+program.command('start').action(require('../lib/start'));
+
+// 选择现在运行的项目
+program.command('select').action(require('../lib/select'));
+
+// 打包选中的项目
+program.command('build').action(require('../lib/build'));
+
+// 上传已经打包的文件
+program.command('upload').action(require('../lib/upload'));
 
 program.parse(process.argv);
